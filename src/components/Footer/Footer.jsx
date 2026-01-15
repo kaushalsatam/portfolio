2b51f0ba5c7e9f5630e1864a7c2d5aaca93ef560
kaushalsatam <kaushalabd17@gmail.com>
2026-01-15 08:56:13 +0530

    Reconstruct entire portfolio website

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    let date = new Date();
-    return <footer>
-        <p className="text-gray-300 text-sm text-center">Copyright © {date.getFullYear()} Kaushal Satam. All Rights Reserved.</p>
-    </footer>
-}
-
-export default Footer;
